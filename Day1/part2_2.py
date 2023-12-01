944126620bb189a652a63ec8fc25ade491dc8a5d
the-aljaz <aljaz@resman.dev>
2023-12-01 19:26:15 +0100

    Day 1

---
+++ Day1/part2_2.py
@@ -0,0 +1,27 @@
+import os
+
+def calculate_calibration_sum(filename):
+    total_sum = 0
+
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line:  # Skip empty lines
+                first_digit = next(char for char in line if char.isdigit())
+                last_digit = next(char for char in reversed(line) if char.isdigit())
+                calibration_value = int(first_digit + last_digit)
+                total_sum += calibration_value
+
+    return total_sum
+
+if __name__ == "__main__":
+    input_filename = "Day1\Output.txt"
+
+    # Print the current working directory
+    print("Current working directory:", os.getcwd())
+
+    # Full path to the input file
+    input_filepath = os.path.join(os.getcwd(), input_filename)
+
+    result = calculate_calibration_sum(input_filepath)
+    print(f"The sum of calibration values is: {result}")
